480f09cddcc4c67ba93d394ff82e219cd75b11bd
kossykappa <kossykappa@msn.com>
2026-04-11 08:45:59 +0200

    webhhok grava pagamentos no supabase

--- api/stripe-webhook.ts
+++ api/stripe-webhook.ts
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 });
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL as string,
+ process.env.SUPABASE_URL as string,
   process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
 
