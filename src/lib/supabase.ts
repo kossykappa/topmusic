f0d07035935e8d54b29266aa94696976a651cc3a
kossykappa <kossykappa@msn.com>
2026-04-28 09:53:42 +0200

    fix supabase connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = https://vmtjtqxaekreuvmkmvhq.supabase.co/rest/v1/
-const supabaseAnonKey = sb_publishable_qkZcVKrj9paT6vxO2AEH6g_Uz9CY0Km
+const supabaseUrl = 'https://vmtjtqxaekreuvmkmvhq.supabase.co/rest/v1/'
+const supabaseAnonKey = 'sb_publishable_qkZcVKrj9paT6vxO2AEH6g_Uz9CY0Km'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
